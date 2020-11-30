a5babd898bdb14e958296bd07e3d0fb704253446
meobanak113 meobanak113 <meobanak8@gmail.com>
2020-12-01 00:28:31 +0700

    some change

--- AutoUpdateDLL/Program.cs
+++ AutoUpdateDLL/Program.cs
@@ -10,6 +10,10 @@ namespace AutoUpdateDLL
     {
         static void Main(string[] args)
         {
+            string connectString = @"Server=nguyentampc\sqlexpress;Database=Application;User Id=sa;Password=19042015;";
+            string dir = "";
+            IAutoUpdateDLL upd = new SQLServer_AutoUpdateDLL(connectString, dir);
+            upd.GetDLLs();
         }
     }
 }
